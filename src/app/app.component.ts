d196138336607c0832b36751ac18d5e541f2a97d
Andrew Albrecht <andrewalbrecht@utexas.edu>
2021-06-24 11:45:37 -0500

    services and pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'myapp';
+  title = 'Hello World how are you?';
+  jsonValue = {
+    a: 'hello',
+    b: 'world'
+  }
+  newDate = new Date()
+  
+  constructor(private httpService: HttpService) {}
+
+  handleEvent(){
+    this.httpService.getRequest('https://jsonplaceholder.typicode.com/posts/1');
+  }
 }
+
